06ca73fcd0abb7e93c92a04a5c06c4912f7d0bdd
Felipe Leon <fglfgl27@gmail.com>
2019-07-03 14:40:58 -0300

    Up master files
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/Play.js
+++ app/specific/Play.js
@@ -458,7 +458,7 @@ function Play_loadDataRequest() {
             (AddUser_UserIsSet() && AddUser_UsernameArray[Main_values.Users_Position].access_token ? '&oauth_token=' +
                 AddUser_UsernameArray[Main_values.Users_Position].access_token : '');
     } else {
-    console.log(Play_tokenResponse.token);
+        console.log(Play_tokenResponse.token);
         console.log(Play_tokenResponse.sig);
         theUrl = 'https://usher.ttvnw.net/api/channel/hls/' + Main_values.Play_selectedChannel +
             '.m3u8?&token=' + encodeURIComponent(Play_tokenResponse.token) + '&sig=' + Play_tokenResponse.sig +

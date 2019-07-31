c3e6a881dea26bf6407437887c3d6d889d641788
Felipe Leon <fglfgl27@gmail.com>
2019-07-30 21:02:54 -0300

    improve screens focus
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/Screens.js
+++ app/specific/Screens.js
@@ -537,7 +537,7 @@ function Screens_addFocusVideo(y, x, idArray, forceScroll) {
         if (y > 0) {
             if (Main_ThumbNull((y + 1), 0, idArray[0])) {
                 Main_ScrollTable(idArray[10],
-                    (document.getElementById(idArray[0] + (y - 1) + '_' + x).offsetTop * -1) + 10 +
+                    (document.getElementById(idArray[0] + (y - 1) + '_' + x).offsetTop * -1) + 15 +
                     (inUseObj.HasSwitches ? 1 : 0));
             } else Main_handleKeyUp();
         } else Main_ScrollTable(idArray[10], screen.height * 0.07);
@@ -551,7 +551,7 @@ function Screens_addFocusChannel(y, x, idArray, forceScroll) {
         if (y > 1) {
             if (Main_ThumbNull((y + 1), 0, idArray[0])) {
                 Main_ScrollTable(idArray[10],
-                    (document.getElementById(idArray[0] + y + '_' + x).offsetTop * -1) + (screen.height * 0.41));
+                    (document.getElementById(idArray[0] + y + '_' + x).offsetTop * -1) + (screen.height * 0.43));
             } else Main_handleKeyUp();
         } else Main_ScrollTable(idArray[10], screen.height * 0.07);
 

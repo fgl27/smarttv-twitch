31bc2445aef2b203ab232a68f7f2d22cdd576490
Felipe Leon <fglfgl27@gmail.com>
2019-12-13 13:03:01 -0300

    Improve PlayClip_loadData410
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/PlayClip.js
+++ app/specific/PlayClip.js
@@ -25,6 +25,7 @@ var PlayClip_PlayerCheckCounter = 0;
 var PlayClip_PlayerCheckQualityChanged = false;
 var PlayClip_PlayerCheckRun = false;
 var PlayClip_Buffer = 2000;
+var PlayClip_loadData410 = false;
 
 var PlayClip_jumpTimers = [0, 5];
 var PlayClip_DurationSeconds = 0;
@@ -155,6 +156,10 @@ function PlayClip_GetStreamerInfoSuccess(response) {
 }
 
 function PlayClip_loadData() {
+    if (PlayClip_loadData410) {
+        PlayClip_loadDataSuccess410();
+        return;
+    }
     PlayClip_loadingDataTry = 0;
     PlayClip_loadingDataTimeout = 2000;
     PlayClip_loadDataRequest();
@@ -176,14 +181,11 @@ function PlayClip_loadDataRequest() {
             if (xmlHttp.status === 200) {
                 PlayClip_QualityGenerate(xmlHttp.responseText);
             } else if (xmlHttp.status === 410) {
-                PlayClip_qualities = [];
-                PlayClip_qualities.push({
-                    'id': 'source | mp4',
-                    'url': ChannelClip_playUrl2
-                });
-
-                PlayClip_state = PlayClip_STATE_PLAYING;
-                PlayClip_qualityChanged();
+                PlayClip_loadData410 = true;
+                window.setTimeout(function() {
+                    PlayClip_loadData410 = false;
+                }, 60 * 60 * 1000);//try again after 1h
+                PlayClip_loadDataSuccess410();
             } else {
                 PlayClip_loadDataError();
             }
@@ -191,6 +193,17 @@ function PlayClip_loadDataRequest() {
     };
 
     xmlHttp.send(null);
+}
+
+function PlayClip_loadDataSuccess410() {
+    PlayClip_qualities = [];
+    PlayClip_qualities.push({
+        'id': 'source | mp4',
+        'url': ChannelClip_playUrl2
+    });
+
+    PlayClip_state = PlayClip_STATE_PLAYING;
+    PlayClip_qualityChanged();
 }
 
 function PlayClip_loadDataError() {

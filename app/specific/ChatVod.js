8af19a1ac83f4306e8329141de0daab94f0ecf0b
fgl27 <fglfgl27@gmail.com>
2019-12-31 11:35:53 -0300

    Update betterttv support
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/specific/ChatVod.js
+++ app/specific/ChatVod.js
@@ -85,7 +85,7 @@ function Chat_loadEmotesError() {
 }
 
 function Chat_loadEmotesSuccess(data) {
-    ChatLive_loadEmotesbbtv(JSON.parse(data));
+    ChatLive_loadEmotesbbtvGlobal(JSON.parse(data));
     Chat_loadEmotesffz();
 }
 

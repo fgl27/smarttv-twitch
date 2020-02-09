474e79c6be9e9d0f740013e76f447d2420d9fbb3
fgl27 <fglfgl27@gmail.com>
2020-02-09 16:33:12 -0300

    Up master files
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- release/githubio/js/master_uncompressed.js
+++ release/githubio/js/master_uncompressed.js
@@ -11687,7 +11687,7 @@
                 if (this.offset && (this.offset + Main_ItemsLimitMax) > this.MaxOffset) this.dataEnded = true;
                 this.url = this.base_url + encodeURIComponent(AddUser_UsernameArray[0].name) + '/follows/games';
 
-                if (this.isLive) this.url += '/live?limit=200';
+                if (this.isLive) this.url += '/live?limit=150';
                 else this.url += '?limit=' + Main_ItemsLimitMax + '&offset=' + this.offset;
             },
             key_refresh: function() {

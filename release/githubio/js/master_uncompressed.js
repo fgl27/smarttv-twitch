570b7b2bab82d30b26e8eb5a8a5ae8576d566bb0
fgl27 <fglfgl27@gmail.com>
2020-03-06 00:51:11 -0300

    Up master files
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- release/githubio/js/master_uncompressed.js
+++ release/githubio/js/master_uncompressed.js
@@ -11029,7 +11029,8 @@
         setMax: function(tempObj) {
             this.MaxOffset = tempObj._total;
 
-            if (typeof this.MaxOffset === 'undefined') {
+            if (!tempObj[this.object]) this.dataEnded = true;
+            else if (typeof this.MaxOffset === 'undefined') {
                 if (tempObj[this.object].length < 90) this.dataEnded = true;
             } else {
                 if (this.data.length >= this.MaxOffset) this.dataEnded = true;

e53971b55b10cbfe83cfc5bc5aecb5f7e0767282
fgl27 <fglfgl27@gmail.com>
2020-12-14 22:06:38 -0300

    Up main files
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- release/githubio/js/master_uncompressed.js
+++ release/githubio/js/master_uncompressed.js
@@ -11798,8 +11798,10 @@
 
         },
         addCell: function(cell) {
+
             var hasLive = this.isLive || this.screen === Main_games;
-            var game = cell.game;
+            var game = this.hasGameProp ? cell.game : cell;
+
             if (!this.idObject[game._id]) {
 
                 this.itemsCount++;
@@ -11828,6 +11830,7 @@
             key_pgDown: Main_Vod,
             key_pgUp: Main_Featured,
             object: 'top',
+            hasGameProp: true,
             base_url: Main_kraken_api + 'games/top?limit=' + Main_ItemsLimitMax,
             set_url: function() {
                 if (this.offset && (this.offset + Main_ItemsLimitMax) > this.MaxOffset) this.dataEnded = true;
@@ -11854,6 +11857,7 @@
             key_pgDown: Main_UserVod,
             key_pgUp: Main_UserHost,
             isLive: false,
+            hasGameProp: true,
             OldUserName: '',
             object: 'follows',
             base_url: Main_kraken_api + 'users/',

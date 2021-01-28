9dc395e6f2396614a44759866d9749bfacd0dd10
fgl27 <fglfgl27@gmail.com>
2021-01-28 04:04:23 -0300

    Improve Play_timeS
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/specific/Play.js
+++ app/specific/Play.js
@@ -1268,6 +1268,7 @@ function Play_lessthanten(time) {
 function Play_timeS(time) {
     var seconds, minutes, hours;
     time += Play_offsettimeMinus / 1000;
+    time = Math.round(Math.max(0, time));
 
     seconds = Play_lessthanten(parseInt(time) % 60);
 

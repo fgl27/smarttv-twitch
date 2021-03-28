ec4ca46eaf2bafbfc53382b93d0c8f186af3b2b0
fgl27 <fglfgl27@gmail.com>
2021-03-28 14:47:16 -0300

    UP main files
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- release/githubio/js/master_uncompressed.js
+++ release/githubio/js/master_uncompressed.js
@@ -6246,7 +6246,7 @@
                     Main_IsNotBrowser = tizen !== null;
                     console.log('Main_IsNotBrowser tizen = ' + Main_IsNotBrowser);
                     Main_IsNotBrowserVersion = '1.0.0';
-                    if (!Main_isReleased) Main_isDebug = true;
+                    Main_isDebug = true;
                 } catch (e) {
                     Main_IsNotBrowserVersion = '1.0.0';
                     Main_IsNotBrowser = 0;

5bbc1615a52e23abc7f7dd82af0d73d29c9e434c
fgl27 <fglfgl27@gmail.com>
2021-04-01 20:20:50 -0300

    Revert "Improve Firebase_CheckScript"
    This reverts commit 507ab237af892f721466e559370a83692a775208.

--- app/general/firebase.js
+++ app/general/firebase.js
@@ -95,10 +95,13 @@ function Firebase_CheckScript(url, AddOnunhandledrejection) {
 
         if (!AddOnunhandledrejection) {
 
-            window.onunhandledrejection = function() {
-                //function(e) {
-                //console.log('onunhandledrejection e ' + e);
-            };
+            var Extra_Script = document.createElement('script'),
+                text = 'window.onunhandledrejection = function(e) {};';
+
+            Extra_Script.setAttribute('type', 'text/javascript');
+            Extra_Script.setAttribute('defer', 'true');
+            Extra_Script.innerHTML = text;
+            document.body.appendChild(Extra_Script);
 
         }
 

055f8278a6945785e738fecdffef0e621ec2b92f
fgl27 <fglfgl27@gmail.com>
2021-04-15 14:14:00 -0300

    Up main files
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- release/githubio/js/master_uncompressed.js
+++ release/githubio/js/master_uncompressed.js
@@ -16823,7 +16823,8 @@
         if (Sidepannel_Pos_Screens[Main_Go]) Main_values.Sidepannel_Pos = Sidepannel_Pos_Screens[Main_Go];
         Sidepannel_UnSetTopOpacity();
 
-        if (Main_values.Sidepannel_Pos && Main_values.Sidepannel_Pos < 9) Main_AddClass('side_panel_new_' + Main_values.Sidepannel_Pos, 'side_panel_new_icons_text');
+        if (Main_values.Sidepannel_Pos && Main_values.Sidepannel_Pos < 8)
+            Main_AddClass('side_panel_new_' + Main_values.Sidepannel_Pos, 'side_panel_new_icons_text');
     }
 
     var Sidepannel_Pos_Screens = [

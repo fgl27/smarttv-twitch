6d05dcb7b589179295df6c32db53aa25e811f63f
Felipe Leon <fglfgl27@gmail.com>
2022-08-15 10:30:19 -0300

    up main files

--- release/githubio/js/master_uncompressed.js
+++ release/githubio/js/master_uncompressed.js
@@ -9651,6 +9651,7 @@
 
             Play_SetFullScreen(Play_isFullScreen);
             Play_avplay.setListener(Play_listener);
+            Play_avplay.setSilentSubtitle(true);
             Play_offsettime = Play_oldcurrentTime;
 
             if (!Main_isReleased) console.log('Before Play_avplay.prepareAsync:', 'date: ' + new Date());
@@ -12241,6 +12242,7 @@
             Play_avplay.setBufferingParam('PLAYER_BUFFER_FOR_RESUME', 'PLAYER_BUFFER_SIZE_IN_SECOND', PlayVod_Buffer);
             Play_SetFullScreen(Play_isFullScreen);
             Play_avplay.setListener(PlayVod_listener);
+            Play_avplay.setSilentSubtitle(true);
 
             Play_avplay.prepareAsync(
                 function() {

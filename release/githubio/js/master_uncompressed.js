423a10aca0f57280d6b925885a03b5bc1411c173
Felipe Leon <fglfgl27@gmail.com>
2023-08-02 12:42:57 -0300

    up main files

--- release/githubio/js/master_uncompressed.js
+++ release/githubio/js/master_uncompressed.js
@@ -12547,6 +12547,8 @@
                 Chat_Pause();
                 if (Main_IsNotBrowser) {
                     Play_avplay.pause();
+                    Play_avplay.stop();
+                    Play_avplay.close();
                     Main_values.vodOffset = parseInt(PlayVod_currentTime / 1000);
                 }
                 Play_ClearPlayer();
@@ -12835,8 +12837,6 @@
                 Play_BufferPercentage = 0;
                 Play_HideBufferDialog();
                 Play_bufferingcomplete = true;
-                // reset the values after using
-                Main_values.vodOffset = 0;
                 if (!Main_isReleased) console.log('onbufferingprogress > 98:', 'date: ' + new Date());
             }
         },
